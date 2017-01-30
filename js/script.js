cc761cc0facc73b513181472946c52aba612e498
Peron Stevens <PeronStevens@outlook.com>
2017-01-29 20:34:13 -0500

    add padding

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $().ready(function(){
     $(".content").fadeIn(4000);
 
     $("#title").typed({
-        strings: ["UNIX enthusiast", "Some guy with a keyboard", "Full Stack Web Developer"],
+        strings: ["UNIX enthusiast", "Some guy with a keyboard", "Likes keyboard shortcuts", "Full Stack Web Developer"],
         typeSpeed: 0,
         showCursor: false
     });
