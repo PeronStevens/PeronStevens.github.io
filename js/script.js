a89e190a69a3754da5f4a62bcf3c16f759c07888
Peron Stevens <peronstevens@yahoo.com>
2017-05-23 14:36:31 -0400

    minor update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
     $(".about").click(function(){
         $("body").toggleClass("change");
-        $(".title, .list, .back, a").toggleClass("white");
+        $(".title, .list, a").toggleClass("white-text");
+        $(".back").toggleClass("white");
     })
     // $(".about").hover(function(){
     //     $("body").toggleClass("change");
