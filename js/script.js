98f54a2f15ef48f1513c3669585e9cdf7096fc2b
Peron Stevens <peronstevens@yahoo.com>
2017-05-23 14:28:33 -0400

    change background

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-    // $(".about").click(function(){
-    //     $("body").toggleClass("change");
-    //     $(".title, .list, .back, a").toggleClass("white");
-    // })
-    $(".about").hover(function(){
+    $(".about").click(function(){
         $("body").toggleClass("change");
         $(".title, .list, .back, a").toggleClass("white");
     })
+    // $(".about").hover(function(){
+    //     $("body").toggleClass("change");
+    //     $(".title, .list, .back, a").toggleClass("white");
+    // })
     // $(".about").on("tap", function(){
     //     $("body").toggleClass("change");
     //     $(".title, .list, .back, a").toggleClass("white");
