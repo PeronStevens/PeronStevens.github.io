799ed42f11a2907f489c6656e057daffade993eb
Peron Stevens <peronstevens@yahoo.com>
2017-05-23 12:59:59 -0400

    touch update

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 $(document).ready(function(){
-    $(".about").click(function(){
-        $("body").toggleClass("change");
-        $(".title, .list, .back, a").toggleClass("white");
-    })
-    $(".about").hover(function(){
-        $("body").toggleClass("change");
-        $(".title, .list, .back, a").toggleClass("white");
+    // $(".about").click(function(){
+    //     $("body").toggleClass("change");
+    //     $(".title, .list, .back, a").toggleClass("white");
+    // })
+    // $(".about").hover(function(){
+    //     $("body").toggleClass("change");
+    //     $(".title, .list, .back, a").toggleClass("white");
+    // })
+    $(function(){
+        $(".about-wrap").bind("tap", tapChange)
+
+        function tapChange(){
+            $("body").toggleClass("change");
+            $(".title, .list, .back, a").toggleClass("white");
+        }
     })
 })
