ff2b29e2d654cf5a05767d62ba3333396872c613
Peron Stevens <peronstevens@yahoo.com>
2017-05-23 14:50:02 -0400

    mobile test

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 $(document).ready(function(){
-    $(".about").click(function(){
+    // $(".about").click(function(){
+    //     $("body").toggleClass("change");
+    //     $(".title, .list, a").toggleClass("white-text");
+    //     $(".back").toggleClass("white");
+    // })
+    $(".about").hover(function(){
         $("body").toggleClass("change");
         $(".title, .list, a").toggleClass("white-text");
         $(".back").toggleClass("white");
     })
-    // $(".about").hover(function(){
-    //     $("body").toggleClass("change");
-    //     $(".title, .list, .back, a").toggleClass("white");
-    // })
-    // $(".about").on("tap", function(){
-    //     $("body").toggleClass("change");
-    //     $(".title, .list, .back, a").toggleClass("white");
-    //     alert();
-    // })
+    $(".about").on("tap", function(){
+        $("body").toggleClass("change");
+        $(".title, .list, a").toggleClass("white-text");
+        $(".back").toggleClass("white");
+    })
 })
