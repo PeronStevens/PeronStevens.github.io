1990a42d0a1763d5b86a810f05dbd50de05a49f1
Peron Stevens <peronstevens@yahoo.com>
2017-05-26 10:27:14 -0400

    ANOTHER REWRITE!

--- js/script.js
+++ js/script.js
@@ -1,25 +1,3 @@
 $(document).ready(function(){
-    $("body").click(function(){
-        $("body, .back").toggleClass("change");
-        $(".title, .list, a").toggleClass("white-text");
-        $(".back").toggleClass("white");
 
-        if ( $(".about").hasClass("about-background") ){
-            $(".about").removeClass("about-background");
-            $(".about").addClass("fire");
-        } else {
-            $(".about").removeClass("fire");
-            $(".about").addClass("about-background");
-        }
-    })
-    // $(".about").hover(function(){
-    //     $("body").toggleClass("change");
-    //     $(".title, .list, a").toggleClass("white-text");
-    //     $(".back").toggleClass("white");
-    // })
-    // $(".about").on("tap", function(){
-    //     $("body").toggleClass("change");
-    //     $(".title, .list, a").toggleClass("white-text");
-    //     $(".back").toggleClass("white");
-    // })
 })
